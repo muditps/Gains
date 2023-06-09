9ca228252c4801455d2533e7d6a04b1ab6a1d236
muditps <muditps13@gmail.com>
2023-06-09 18:58:26 +0530

    first commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '9ba539354cmsh662e24aae5fe6fep1ec52cjsn5d4458cd9052',
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+  },
+}
+  
+  export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+      'X-RapidAPI-Key': '9ba539354cmsh662e24aae5fe6fep1ec52cjsn5d4458cd9052',
+    },
+  };
+  
+  export const fetchData = async (url, options) => {
+    const res = await fetch(url, options);
+    const data = await res.json();
+  
+    return data;
+  };
